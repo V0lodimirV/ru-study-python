5a374744fcd4dc8f3997e169831bdbeb87fc19b3
VolodimirV <vladimir_veko@mail.ru>
2023-03-06 12:28:15 +0300

    add file lists.py and test_lists.py

--- lists/lists.py
+++ lists/lists.py
@@ -1,23 +1,42 @@
 class ListExercise:
     @staticmethod
     def replace(input_list: list[int]) -> list[int]:
-        """
-        Заменить все положительные элементы целочисленного списка на максимальное значение
-        элементов списка.
+        def replace_helper(input_list, max_val):
+            if not input_list:
+                return []
+            new_list = replace_helper(input_list[1:], max_val)
 
-        :param input_list: Исходный список
-        :return: Список с замененными элементами
-        """
-        pass
+            if input_list[0] > 0:
+                input_list[0] = max_val
+
+            return [input_list[0]] + new_list
+
+        if not input_list:
+            return []
+
+        max_val = max(input_list)
+
+        return replace_helper(input_list, max_val)
 
     @staticmethod
     def search(input_list: list[int], query: int) -> int:
-        """
-        Реализовать двоичный поиск
-        Функция должна возвращать индекс элемента
+        def search_helper(input_list, query, start, end):
 
-        :param input_list: Исходный список
-        :param query: Искомый элемент
-        :return: Номер элемента
-        """
-        pass
+            if start >= end:
+                return -1
+
+            mid = (start + end) // 2
+
+            if input_list[mid] == query:
+                return mid
+
+            elif input_list[mid] > query:
+                return search_helper(input_list, query, start, mid)
+
+            else:
+                return search_helper(input_list, query, mid + 1, end)
+
+        if not input_list:
+            return -1
+
+        return search_helper(input_list, query, 0, len(input_list))
